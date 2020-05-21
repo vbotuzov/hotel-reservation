17ff2802b087e76f9785a9f892fb216073c62950
vbotuzov <64745193+vbotuzov@users.noreply.github.com>
2020-05-21 16:53:20 +0300

    Update Facade.cpp

--- Facade/Facade/Facade.cpp
+++ Facade/Facade/Facade.cpp
@@ -4,39 +4,39 @@ using namespace std;
 
 class Room1 {
 public:
-	std::string Type() const {
+	string Type() const {
 		return "Lux\n";
 	}
-	std::string Capacity() const {
+	string Capacity() const {
 		return "3 person\n";
 	}
-	std::string Prize() const {
+	string Prize() const {
 		return "1000$\n";
 	}
 };
 
 class Room2 {
 public:
-	std::string Type() const {
+	string Type() const {
 		return "Middle\n";
 	}
-	std::string Capacity() const {
+	string Capacity() const {
 		return "2 person\n";
 	}
-	std::string Prize() const {
+	string Prize() const {
 		return "750$\n";
 	}
 };
 
 class Room3 {
 public:
-	std::string Type() const {
+	string Type() const {
 		return "Usual\n";
 	}
-	std::string Capacity() const {
+	string Capacity() const {
 		return "1 person\n";
 	}
-	std::string Prize() const {
+	string Prize() const {
 		return "500$\n";
 	}
 };
@@ -58,16 +58,22 @@ public:
 		delete room2_;
 		delete room3_;
 	}
-	std::string Operation() {
-		std::string result = "Room1:\n";
+	string Operation1() {
+		string result = "Room1:\n";
 		result += this->room1_->Type();
 		result += this->room1_->Capacity();
 		result += this->room1_->Prize();
-		result += "Room2:\n";
+		return result;
+	}
+	string Operation2() {
+		string result = "Room2:\n";
 		result += this->room2_->Type();
 		result += this->room2_->Capacity();
 		result += this->room2_->Prize();
-		result += "Room3:\n";
+		return result;
+	}
+	string Operation3(){
+		string result = "Room3:\n";
 		result += this->room3_->Type();
 		result += this->room3_->Capacity();
 		result += this->room3_->Prize();
@@ -75,15 +81,17 @@ public:
 	}
 };
 
-void ClientCode(Facade *facade) {
-	std::cout << facade->Operation();
+void Client(Facade *facade) {
+	cout << facade->Operation1();
+	cout << facade->Operation2();
+	cout << facade->Operation3();
 }
 int main() {
 	Room1 *room1 = new Room1;
 	Room2 *room2 = new Room2;
 	Room3 *room3 = new Room3;
 	Facade *facade = new Facade(room1, room2, room3);
-	ClientCode(facade);
+	Client(facade);
 	delete facade;
 	system("pause");
 	return 0;
