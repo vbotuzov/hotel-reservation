f1d3c7b2141d029a22c8e444825e44c927b12d9e
vbotuzov <64745193+vbotuzov@users.noreply.github.com>
2020-06-03 17:06:26 +0300

    DAL
    Added DAL project

---
+++ OSBB/OSBB/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OSBB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
